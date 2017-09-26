05f54b0cd2a831729e2b8c757cc72557a437db13
Nicole <nikybg96@gmail.com>
2017-09-26 12:49:27 -0400

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from "./components/demo/demo.component";
+import {Demo1Component} from "./components/demo1/demo1.component";
+import {AbilitiesComponent} from "./components/abilities/abilities.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'demo',component: DemoComponent
+  },
+  {
+    path: 'demo1',component: Demo1Component
+  },
+  {
+    path: 'abilities',component: AbilitiesComponent
+  },
+  {
+    path: 'contact',component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { Demo1Component } from './components/demo1/demo1.component';
+import { AbilitiesComponent } from './components/abilities/abilities.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DemoComponent,
+    Demo1Component,
+    AbilitiesComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
